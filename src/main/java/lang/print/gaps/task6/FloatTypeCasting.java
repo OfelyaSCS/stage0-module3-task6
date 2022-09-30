e2abdf18cf5ed35565cfc97f750a141099385b8a
OfelyaSCS <sahakyanscofelya@gmail.com>
2022-09-30 17:43:18 +0400

    stage0-module3-task6

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,20 @@
 package lang.print.gaps.task6;
 
+import java.util.Scanner;
+
 public class FloatTypeCasting {
+    int number;
     public void roundNumber(float numberToBeRounded) {
+        number=(int)numberToBeRounded;
+    }
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter float number ");
+        float floatNumber=input.nextFloat();
+
+        FloatTypeCasting floatTypeCasting=new FloatTypeCasting();
+        floatTypeCasting.roundNumber(floatNumber);
+        System.out.println("Rounded number is "+floatTypeCasting.number);
     }
 }
